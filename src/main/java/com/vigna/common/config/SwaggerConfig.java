18750a002f3c1208e69096b77640078c261b650c
Azir <2075125282@qq.com>
2024-04-17 00:50:57 +0800

    feat: Swagger支持自定义方法名以排除必填的请求头Authorization校验

--- src/main/java/com/vigna/common/config/SwaggerConfig.java
+++ src/main/java/com/vigna/common/config/SwaggerConfig.java
@@ -15,7 +15,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Swagger 配置
@@ -29,42 +30,39 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .components(
-                    new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                            new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .info(new Info().title("上流男士高端发言记录系统")
-                        .description("管理记录高端IT男士的优雅发言")
-                        .version("v0.0.1")
-                        .license(new License().name("私有项目，不允许任何借鉴或商用").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("上流男士高端发言管理 Wiki Documentation")
-                        .url("https://springshop.wiki.github.org/docs"));
+            .components(
+                new Components()
+                    .addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                    )
+            )
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .info(new Info().title("Vigna API")
+                .description("Vigna API 接口文档")
+                .version("v0.0.1")
+                .license(new License().name("MIT License").url("https://opensource.org/licenses/MIT")))
+            .externalDocs(new ExternalDocumentation()
+                .description("no wiki")
+                .url("Your own wiki or homepage"));
     }
+
+    // 排除的Authorization的方法名
+    private final Set<String> excludedAuthorizationMethodName = new HashSet<>(Arrays.asList("login", "register"));
 
     @Bean
     public OperationCustomizer addGlobalHeaderParameter() {
         return (operation, handlerMethod) -> {
-            // 排除的接口列表
-            List<String> excludeSummary = Arrays.asList("用户注册", "用户登录");
-
-            // 获取当前接口的Summary
-            String currentSummary = operation.getSummary();
-
-            // 如果当前接口不在排除的接口列表中，就添加token参数
-            if (!excludeSummary.contains(currentSummary)) {
-                Parameter tokenParameter = new PathParameter().in("header").name("Authorization").required(true).description("Bearer token");
-                if (operation.getParameters() == null) {
-                    operation.addParametersItem(tokenParameter);
-                } else if (operation.getParameters().stream().noneMatch(parameter -> parameter.getName().equals("token"))) {
-                    operation.addParametersItem(tokenParameter);
-                }
+            String MethodName = handlerMethod.getMethod().getName();
+            if (!excludedAuthorizationMethodName.contains(MethodName)) {
+                Parameter tokenParameter = new PathParameter()
+                    .in("header")
+                    .name("Authorization")
+                    .required(true)
+                    .description("Bearer token");
+                operation.addParametersItem(tokenParameter);
             }
             return operation;
         };
